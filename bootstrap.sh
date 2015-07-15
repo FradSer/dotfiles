7ecf630341005099092a7bf19a40586502e23d22
Frad Lee <fradser@gmail.com>
2015-07-15 15:38:19 +0800

    Add spf13-vim script

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,6 +8,7 @@ function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
 		--exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
 	source ~/.bash_profile;
+    curl https://j.mp/spf13-vim3 -L > spf13-vim.sh && sh spf13-vim.sh;
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
