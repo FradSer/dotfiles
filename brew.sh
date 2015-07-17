3936b536515869e0016aa17fa19b0900e60c43eb
Frad Lee <fradser@gmail.com>
2015-07-18 01:43:37 +0800

    Add command to tonsatll RVM and stable Ruby

--- brew.sh
+++ brew.sh
@@ -60,6 +60,11 @@ n stable
 brew remove node
 brew cleanup
 
-# Insatll RVM & stable Ruby
+# Insatll RVM and stable Ruby
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 \curl -sSL https://get.rvm.io | bash -s stable --ruby
+rm .profile
+echo "# RVM" >> ~/.bash_profile
+echo "export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting" >> ~/.bash_profile
+echo -e '\n' >> ~/.bash_profile
+echo "[[ -s "$HOME/.rvm/scripts/rvm"  ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*" >> ~/.bash_profile
